741a9ad0e2944a08b3942627afeb7ea0efa8d1ff
devros42 <github@devros.com>
2017-06-12 11:00:48 -0400

    Create start.sh

--- start.sh
+++ start.sh
@@ -23,6 +23,8 @@ fi
 
 munin-node-configure --remove --shell | sh
 ln -s /usr/share/munin/plugins/hddtemp_smartctl   /etc/munin/plugins/hddtemp_smartctl
+ln -s /usr/share/munin/plugins/iostat   /etc/munin/plugins/iostat 
+ln -s /usr/share/munin/plugins/meminfo   /etc/munin/plugins/meminfo 
 
 service cron start
 a2enmod cgid
